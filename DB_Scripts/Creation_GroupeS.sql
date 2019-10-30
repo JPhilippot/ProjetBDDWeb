1d7822f0f9a2cf60f673f3aaf62a202078f3df09
Aurelien Besnier <aurelienbesnier7320@gmail.com>
2019-10-30 13:48:10 +0100

    Update Creation

--- DB_Scripts/Creation_GroupeS.sql
+++ DB_Scripts/Creation_GroupeS.sql
@@ -5,6 +5,13 @@ Aurélien Besnier 21709012
 Josua Philippot 21703792
 Nom du groupe : S
 */
+/*
+Fichier : Creation_GroupeS.sql
+Auteurs:
+Aurélien Besnier 21709012
+Josua Philippot 21703792
+Nom du groupe : S
+*/
 
 #------------------------------------------------------------
 #        Script MySQL.
@@ -16,11 +23,10 @@ Nom du groupe : S
 #------------------------------------------------------------
 
 CREATE TABLE Administrateur(
-        ID_Admin Int  Auto_increment  NOT NULL ,
-        email    Text NOT NULL ,
         login    Varchar (50) NOT NULL ,
+        email    Text NOT NULL ,
         password Varchar (80) NOT NULL
-	,CONSTRAINT Administrateur_PK PRIMARY KEY (ID_Admin)
+	,CONSTRAINT Administrateur_PK PRIMARY KEY (login)
 )ENGINE=InnoDB;
 
 
@@ -29,11 +35,10 @@ CREATE TABLE Administrateur(
 #------------------------------------------------------------
 
 CREATE TABLE Visiteur(
-        ID_Vis   Int  Auto_increment  NOT NULL ,
-        email    Text NOT NULL ,
         login    Varchar (20) NOT NULL ,
+        email    Text NOT NULL ,
         password Varchar (80) NOT NULL
-	,CONSTRAINT Visiteur_PK PRIMARY KEY (ID_Vis)
+	,CONSTRAINT Visiteur_PK PRIMARY KEY (login)
 )ENGINE=InnoDB;
 
 
@@ -42,14 +47,13 @@ CREATE TABLE Visiteur(
 #------------------------------------------------------------
 
 CREATE TABLE Contributeur(
-        ID_Contrib Int  Auto_increment  NOT NULL ,
-        email      Text NOT NULL ,
-        login      Varchar (20) NOT NULL ,
-        password   Varchar (80) NOT NULL ,
-        ID_Admin   Int
-	,CONSTRAINT Contributeur_PK PRIMARY KEY (ID_Contrib)
+        login                Varchar (20) NOT NULL ,
+        email                Text NOT NULL ,
+        password             Varchar (80) NOT NULL ,
+        login_Administrateur Varchar (50)
+	,CONSTRAINT Contributeur_PK PRIMARY KEY (login)
 
-	,CONSTRAINT Contributeur_Administrateur_FK FOREIGN KEY (ID_Admin) REFERENCES Administrateur(ID_Admin)
+	,CONSTRAINT Contributeur_Administrateur_FK FOREIGN KEY (login_Administrateur) REFERENCES Administrateur(login)
 )ENGINE=InnoDB;
 
 
@@ -71,13 +75,13 @@ CREATE TABLE Localisation(
 #------------------------------------------------------------
 
 CREATE TABLE Theme(
-        Nom                     Varchar (20) NOT NULL ,
-        ID_Admin                Int ,
-        ID_Admin_Administrateur Int NOT NULL
+        Nom                  Varchar (20) NOT NULL ,
+        login                Varchar (50) ,
+        login_Administrateur Varchar (50) NOT NULL
 	,CONSTRAINT Theme_PK PRIMARY KEY (Nom)
 
-	,CONSTRAINT Theme_Administrateur_FK FOREIGN KEY (ID_Admin) REFERENCES Administrateur(ID_Admin)
-	,CONSTRAINT Theme_Administrateur0_FK FOREIGN KEY (ID_Admin_Administrateur) REFERENCES Administrateur(ID_Admin)
+	,CONSTRAINT Theme_Administrateur_FK FOREIGN KEY (login) REFERENCES Administrateur(login)
+	,CONSTRAINT Theme_Administrateur0_FK FOREIGN KEY (login_Administrateur) REFERENCES Administrateur(login)
 )ENGINE=InnoDB;
 
 
@@ -87,16 +91,17 @@ CREATE TABLE Theme(
 
 CREATE TABLE Evenement(
         ID_Event       Int  Auto_increment  NOT NULL ,
+        Titre          Varchar (50) NOT NULL ,
         Date           Date NOT NULL ,
         EffectifMax    Int NOT NULL ,
         Descriptif     Text NOT NULL ,
         EffectifActuel Int NOT NULL ,
-        ID_Contrib     Int NOT NULL ,
+        login          Varchar (20) NOT NULL ,
         ID_Loc         Int NOT NULL ,
         Nom            Varchar (20) NOT NULL
 	,CONSTRAINT Evenement_PK PRIMARY KEY (ID_Event)
 
-	,CONSTRAINT Evenement_Contributeur_FK FOREIGN KEY (ID_Contrib) REFERENCES Contributeur(ID_Contrib)
+	,CONSTRAINT Evenement_Contributeur_FK FOREIGN KEY (login) REFERENCES Contributeur(login)
 	,CONSTRAINT Evenement_Localisation0_FK FOREIGN KEY (ID_Loc) REFERENCES Localisation(ID_Loc)
 	,CONSTRAINT Evenement_Theme1_FK FOREIGN KEY (Nom) REFERENCES Theme(Nom)
 )ENGINE=InnoDB;
@@ -107,14 +112,14 @@ CREATE TABLE Evenement(
 #------------------------------------------------------------
 
 CREATE TABLE Supprime_Event(
-        ID_Admin   Int NOT NULL ,
-        ID_Event   Int NOT NULL ,
-        ID_Contrib Int NOT NULL
-	,CONSTRAINT Supprime_Event_PK PRIMARY KEY (ID_Admin,ID_Event,ID_Contrib)
+        login              Varchar (50) NOT NULL ,
+        ID_Event           Int NOT NULL ,
+        login_Contributeur Varchar (20) NOT NULL
+	,CONSTRAINT Supprime_Event_PK PRIMARY KEY (login,ID_Event,login_Contributeur)
 
-	,CONSTRAINT Supprime_Event_Administrateur_FK FOREIGN KEY (ID_Admin) REFERENCES Administrateur(ID_Admin)
+	,CONSTRAINT Supprime_Event_Administrateur_FK FOREIGN KEY (login) REFERENCES Administrateur(login)
 	,CONSTRAINT Supprime_Event_Evenement0_FK FOREIGN KEY (ID_Event) REFERENCES Evenement(ID_Event)
-	,CONSTRAINT Supprime_Event_Contributeur1_FK FOREIGN KEY (ID_Contrib) REFERENCES Contributeur(ID_Contrib)
+	,CONSTRAINT Supprime_Event_Contributeur1_FK FOREIGN KEY (login_Contributeur) REFERENCES Contributeur(login)
 )ENGINE=InnoDB;
 
 
@@ -124,16 +129,15 @@ CREATE TABLE Supprime_Event(
 
 CREATE TABLE S_inscrit(
         ID_Event Int NOT NULL ,
-        ID_Vis   Int NOT NULL
-	,CONSTRAINT S_inscrit_PK PRIMARY KEY (ID_Event,ID_Vis)
+        login    Varchar (20) NOT NULL
+	,CONSTRAINT S_inscrit_PK PRIMARY KEY (ID_Event,login)
 
 	,CONSTRAINT S_inscrit_Evenement_FK FOREIGN KEY (ID_Event) REFERENCES Evenement(ID_Event)
-	,CONSTRAINT S_inscrit_Visiteur0_FK FOREIGN KEY (ID_Vis) REFERENCES Visiteur(ID_Vis)
+	,CONSTRAINT S_inscrit_Visiteur0_FK FOREIGN KEY (login) REFERENCES Visiteur(login)
 )ENGINE=InnoDB;
 
-ALTER TABLE Visiteur AUTO_INCREMENT=1;
+
 ALTER TABLE Evenement AUTO_INCREMENT=1;
-ALTER TABLE Contributeur AUTO_INCREMENT=1;
 ALTER TABLE Localisation AUTO_INCREMENT=1;
 
 
@@ -142,8 +146,4 @@ ALTER TABLE Localisation AUTO_INCREMENT=1;
 #------------------------------------------------------------
 
 
-INSERT INTO Administrateur SELECT 1,'admin@email.com','root',SHA2('root',256);
 
-INSERT INTO Localisation VALUES(1,'MONTPELLIER',43.6119, 3.8772);
-
-INSERT INTO Theme VALUES('Concert',1,1);
